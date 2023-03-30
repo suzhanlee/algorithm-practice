b811e9af3efdb3b0e19d606623e0b0b44388cd33
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-03-30 20:47:49 +0900

    시뮬레이션-구현 : 사다리 타기

---
+++ src/자바_코딩테스트/시뮬레이션_구현/사다리타기.java
@@ -0,0 +1,59 @@
+package 자바_코딩테스트.시뮬레이션_구현;
+
+import java.util.Arrays;
+
+class Solution {
+
+    static int[][] ladder;
+
+    public char[] solution(int n, int[][] ladder) {
+        char[] answer = new char[n];
+
+        this.ladder = ladder;
+
+        for (int i = 1; i <= n; i++) {
+
+            int i1 = 'A' + i - 1;
+            char c = (char) i1;
+            int location = move(i);
+            answer[location - 1] = c;
+        }
+
+        return answer;
+    }
+
+    public static int move(int num) {
+
+        for (int i = 0; i < ladder.length; i++) {
+
+            for (int j = 0; j < ladder[i].length; j++) {
+                int x = ladder[i][j];
+                if(num == x) {
+                    num++;
+                    break;
+                }
+
+                if(num == x + 1) {
+                    num--;
+                    break;
+
+                }
+            }
+
+        }
+
+        return num;
+    }
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        System.out.println(Arrays.toString(T.solution(5, new int[][]{{1, 3}, {2, 4}, {1, 4}})));
+        System.out.println(
+            Arrays.toString(T.solution(7, new int[][]{{1, 3, 5}, {1, 3, 6}, {2, 4}})));
+        System.out.println(
+            Arrays.toString(T.solution(8, new int[][]{{1, 5}, {2, 4, 7}, {1, 5, 7}, {2, 5, 7}})));
+        System.out.println(Arrays.toString(T.solution(12,
+            new int[][]{{1, 5, 8, 10}, {2, 4, 7}, {1, 5, 7, 9, 11}, {2, 5, 7, 10},
+                {3, 6, 8, 11}})));
+    }
+}
