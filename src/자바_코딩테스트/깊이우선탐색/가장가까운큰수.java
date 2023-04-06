235a5123bfc8cd2d58794db382966204b58ee3fc
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-04-06 15:44:44 +0900

    깊이우선탐색 : 가장 가까운 큰수 코드 수정

--- src/자바_코딩테스트/깊이우선탐색/가장가까운큰수.java
+++ src/자바_코딩테스트/깊이우선탐색/가장가까운큰수.java
@@ -1,49 +1,73 @@
 package 자바_코딩테스트.깊이우선탐색;
 
-import java.util.*;
+import java.util.Arrays;
+
 class 가장가까운큰수 {
-    int answer, target, m;
-    ArrayList<Integer> nums;
-    int[] ch;
-    boolean flag;
-    public void DFS(int L, int number){
-        if(flag) return;
-        if(L == m){
-            if(number > target){
-                answer = number;
-                flag = true;
-            }
-        }
-        else{
-            for(int i = 0; i < m; i++){
-                if(ch[i] == 0){
-                    ch[i] = 1;
-                    DFS(L + 1, number * 10 + nums.get(i));
-                    ch[i] = 0;
-                }
-            }
-        }
-    }
-    public int solution(int n){
+
+    static int length;
+    static int[] nums;
+    static int n;
+    static boolean flag = false;
+    static int answer;
+    static int[] visited;
+
+    public int solution(int n) {
+        this.n = n;
         answer = 0;
-        flag = false;
-        nums = new ArrayList<>();
-        target = n;
-        int tmp = n;
-        while(tmp > 0){
-            int t = tmp%10;
-            nums.add(t);
-            tmp = tmp / 10;
+
+        length = String.valueOf(n).length();
+        nums = new int[length];
+
+        int value = n;
+        visited = new int[length];
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+
+            int x = (int) (value / (Math.pow(10, i)));
+            nums[nums.length - 1 - i] = x;
+
+            value -= (x * Math.pow(10, i));
         }
-        nums.sort((a, b) -> a - b);
-        m = nums.size();
-        ch = new int[m];
-        DFS(0, 0);
-        if(flag == false) return -1;
+
+        Arrays.sort(nums);
+
+        dfs(0, 0);
+
+        if (answer == 0) {
+            return -1;
+        }
+
         return answer;
     }
 
-    public static void main(String[] args){
+    public static void dfs(int N, int value) {
+
+        if (N == length) {
+
+            if (flag == true && value > n) {
+                answer = value;
+                flag = false;
+                return;
+            }
+
+            if (value == n) {
+                flag = true;
+            }
+
+        } else {
+            for (int i = 0; i < length; i++) {
+                if (visited[i] == 0) {
+                    visited[i] = 1;
+                    dfs(N + 1, value * 10 + nums[i]);
+                    visited[i] = 0;
+                }
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) {
         가장가까운큰수 T = new 가장가까운큰수();
         System.out.println(T.solution(123));
         System.out.println(T.solution(321));
