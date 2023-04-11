1d730525688ca9078fa7cca396c8964065c2288c
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-04-11 19:01:06 +0900

    사다리 타기 코드 수정

--- src/자바_코딩테스트/시뮬레이션_구현/사다리타기.java
+++ src/자바_코딩테스트/시뮬레이션_구현/사다리타기.java
@@ -1,75 +1,28 @@
 package 자바_코딩테스트.시뮬레이션_구현;
 
-import java.util.Arrays;
-
-<<<<<<< HEAD
 class 사다리타기 {
-=======
-class Solution {
->>>>>>> origin/main
-
-    static int[][] ladder;
 
     public char[] solution(int n, int[][] ladder) {
+
         char[] answer = new char[n];
 
-        this.ladder = ladder;
-
-        for (int i = 1; i <= n; i++) {
-
-            int i1 = 'A' + i - 1;
-            char c = (char) i1;
-            int location = move(i);
-            answer[location - 1] = c;
+        for (int i = 0; i < n; i++) {
+            answer[i] = (char) (i + 65);
         }
 
-        return answer;
-    }
+        for (int[] line : ladder) {
 
-    public static int move(int num) {
-
-        for (int i = 0; i < ladder.length; i++) {
-
-            for (int j = 0; j < ladder[i].length; j++) {
-                int x = ladder[i][j];
-<<<<<<< HEAD
-                if (num == x) {
-=======
-                if(num == x) {
->>>>>>> origin/main
-                    num++;
-                    break;
-                }
-
-<<<<<<< HEAD
-                if (num == x + 1) {
-=======
-                if(num == x + 1) {
->>>>>>> origin/main
-                    num--;
-                    break;
-
-                }
+            for (int x : line) {
+                char tmp = answer[x];
+                answer[x] = answer[x - 1];
+                answer[x - 1] = tmp;
             }
 
         }
 
-        return num;
-    }
+        return answer;
 
-    public static void main(String[] args) {
-<<<<<<< HEAD
-        사다리타기 T = new 사다리타기();
-=======
-        Solution T = new Solution();
->>>>>>> origin/main
-        System.out.println(Arrays.toString(T.solution(5, new int[][]{{1, 3}, {2, 4}, {1, 4}})));
-        System.out.println(
-            Arrays.toString(T.solution(7, new int[][]{{1, 3, 5}, {1, 3, 6}, {2, 4}})));
-        System.out.println(
-            Arrays.toString(T.solution(8, new int[][]{{1, 5}, {2, 4, 7}, {1, 5, 7}, {2, 5, 7}})));
-        System.out.println(Arrays.toString(T.solution(12,
-            new int[][]{{1, 5, 8, 10}, {2, 4, 7}, {1, 5, 7, 9, 11}, {2, 5, 7, 10},
-                {3, 6, 8, 11}})));
     }
 }
+
+
