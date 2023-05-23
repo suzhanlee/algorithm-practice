edb58eb14ec59edd11ff98dd6fff0d04d8b99bed
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-05-23 19:20:29 +0900

    dfs문제풀이

---
+++ src/코테대비52문제/dfs/IP주소.java
@@ -0,0 +1,25 @@
+package 코테대비52문제.dfs;
+
+import java.util.*;
+public class IP주소 {
+
+    public String[] solution(String s){
+
+        String[] answer = {};
+
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+        IP주소 T = new IP주소();
+
+        System.out.println(Arrays.toString(T.solution("2025505")));
+        System.out.println(Arrays.toString(T.solution("0000")));
+        System.out.println(Arrays.toString(T.solution("255003")));
+        System.out.println(Arrays.toString(T.solution("155032012")));
+        System.out.println(Arrays.toString(T.solution("02325123")));
+        System.out.println(Arrays.toString(T.solution("121431211")));
+    }
+}
