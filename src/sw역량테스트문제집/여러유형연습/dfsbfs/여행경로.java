66d30ed5aed1cff67945e48eb9bff55b6cbe4ae0
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-07-18 23:16:06 +0900

    여행경로 수정

---
+++ src/sw역량테스트문제집/여러유형연습/dfsbfs/여행경로.java
@@ -0,0 +1,16 @@
+package sw역량테스트문제집.여러유형연습.dfsbfs;
+
+import java.math.BigInteger;
+
+public class 여행경로 {
+
+    public static void main(String[] args) {
+
+        BigInteger bigInteger = new BigInteger("100");
+        bigInteger.negate();
+
+
+
+    }
+
+}
